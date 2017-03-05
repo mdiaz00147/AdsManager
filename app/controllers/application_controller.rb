047c1bf98c26af874bcf3320357a62b378358f8d
mdiaz00147 <mdiaz00147@gmail.com>
2017-03-05 11:28:18 -0500

    counted

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::API
 	before_action :add_allow_credentials_headers
   	def index
-  		
+  		count_ad	=	CountryConfig.joins(:ads_hits).select(:id,:iso,:name,'ads_hits.ad_id as ad_id','ads_hits.country_config_id as country_config_id').all.group(:iso)
+  		# .select(:id,'lessons.start_date as start_date',:options,:user_id,:lesson_id)
+  		render json: count_ad.size
   	end
 	def add_allow_credentials_headers                                                                                                                                                                                                                                                        
 		response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'                                                                                                                                                                                                     
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   resources :ads_relations
   resources :ads
   root 'ads#index'
+
   get	'/countries', to: 'country_configs#index'
   get 	'/countries/count', to: 'country_configs#index_count'
   get 	'/country/ads/:iso', to: 'country_configs#show'
   get	'/ads/action/count', to: 'ads#index_count'
+
+  get 	'/test', to: 'application#index'
 end
